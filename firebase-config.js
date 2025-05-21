48e0d46a4496c7b67d55f378993925a563bc3a1d
yuki.k <t-kondoy@e.osakamanabi.jp>
2025-05-22 06:32:51 +0900

    最初のコミット：Firebase席決めアプリ

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBy9T1n-iW949p4h0sJnpLtRBfVp6nelc",
+  authDomain: "seat-app-f68f7.firebaseapp.com",
+  databaseURL: "https://seat-app-f68f7-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "seat-app-f68f7",
+  storageBucket: "seat-app-f68f7.appspot.com",
+  messagingSenderId: "1100194214831",
+  appId: "1:1100194214831:web:1c04547c951acf37f20d9a",
+  measurementId: "G-8PV3BT80GS"
+};
+
+// v8 スタイルで初期化
+firebase.initializeApp(firebaseConfig);
